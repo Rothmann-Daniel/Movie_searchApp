9b608fdc69ab45e0fae6527b1eeaeafa1b807416
Daniel Rotman <danilarothmann@gmail.com>
2025-07-11 19:22:14 +0700

    fixing memory leak issues add OkHttpClient

--- app/src/main/java/com/example/uithread/presentation/movies/MoviesSearchPresenter.kt
+++ app/src/main/java/com/example/uithread/presentation/movies/MoviesSearchPresenter.kt
@@ -10,15 +10,16 @@ import com.example.uithread.ui.movies.model.MoviesState
 import com.example.uithread.util.Creator
 
 class MoviesSearchPresenter(
-    private var view: MoviesView? = null,
     private val context: Context,
 ) {
 
+    private var view: MoviesView? = null
+    private var lastSearchText: String? = null
+    private var latestSearchText: String? = null
     private val moviesInteractor = Creator.provideMoviesInteractor(context)
     private val handler = Handler(Looper.getMainLooper())
-    private val movies = mutableListOf<Movie>()
+    private var state: MoviesState? = null
 
-    private var lastSearchText: String? = null
 
     private val searchRunnable = Runnable {
         val newSearchText = lastSearchText ?: ""
@@ -27,6 +28,7 @@ class MoviesSearchPresenter(
 
     fun attachView(view: MoviesView) {
         this.view = view
+        state?.let { view.render(it) }
     }
 
     fun detachView() {
@@ -34,6 +36,9 @@ class MoviesSearchPresenter(
     }
 
     fun searchDebounce(changedText: String) {
+        if (latestSearchText == changedText) {
+            return
+        }
         this.lastSearchText = changedText
         handler.removeCallbacks(searchRunnable)
         handler.postDelayed(searchRunnable, SEARCH_DEBOUNCE_DELAY)
@@ -42,22 +47,21 @@ class MoviesSearchPresenter(
 
     private fun searchRequest(newSearchText: String) {
         if (newSearchText.isNotEmpty()) {
-            view?.render(
-                MoviesState.Loading
-            )
-
+            renderState(MoviesState.Loading)
+            try {
             moviesInteractor.searchMovies(newSearchText, object : MoviesInteractor.MoviesConsumer {
                 override fun consume(foundMovies: List<Movie>?, errorMessage: String?) {
                     handler.post {
+                        val movies = mutableListOf<Movie>()
                         if (foundMovies != null) {
-                            movies.clear()
                             movies.addAll(foundMovies)
                         }
 
                         when {
                             errorMessage != null -> {
-                                view?.render(
+                                renderState(
                                     MoviesState.Error(
+
                                         errorMessage = context.getString(R.string.something_went_wrong),
                                     )
                                 )
@@ -65,7 +69,7 @@ class MoviesSearchPresenter(
                             }
 
                             movies.isEmpty() -> {
-                                view?.render(
+                                renderState(
                                     MoviesState.Empty(
                                         message = context.getString(R.string.nothing_found),
                                     )
@@ -73,7 +77,7 @@ class MoviesSearchPresenter(
                             }
 
                             else -> {
-                                view?.render(
+                                renderState(
                                     MoviesState.Content(
                                         movies = movies,
                                     )
@@ -84,9 +88,23 @@ class MoviesSearchPresenter(
                     }
                 }
             })
+        } catch (e: Exception) {
+            handler.post {
+                renderState(
+                    MoviesState.Error(
+                        errorMessage = context.getString(R.string.network_error)
+                    )
+                )
+            }
         }
     }
 
+    }
+
+    private fun renderState(state: MoviesState) {
+        this.state = state
+        this.view?.render(state)
+    }
 
     companion object {
         private const val SEARCH_DEBOUNCE_DELAY = 2000L
