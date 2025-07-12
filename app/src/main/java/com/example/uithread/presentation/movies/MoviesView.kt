ba6089a5df931210703040d453457d1598dd6512
Daniel Rotman <danilarothmann@gmail.com>
2025-07-12 21:21:57 +0700

    add implementation Moxy

--- app/src/main/java/com/example/uithread/presentation/movies/MoviesView.kt
+++ app/src/main/java/com/example/uithread/presentation/movies/MoviesView.kt
@@ -2,28 +2,36 @@ package com.example.uithread.presentation.movies
 
 import com.example.uithread.domain.models.Movie
 import com.example.uithread.ui.movies.model.MoviesState
+import moxy.MvpView
+import moxy.viewstate.strategy.AddToEndSingleStrategy
+import moxy.viewstate.strategy.OneExecutionStateStrategy
+import moxy.viewstate.strategy.StateStrategyType
 
-interface MoviesView {
+
+interface MoviesView: MvpView {
 
     // Методы, меняющие внешний вид экрана
+    @StateStrategyType(AddToEndSingleStrategy::class)
+    fun render(state: MoviesState)
 
     // Состояние загрузки
+    @StateStrategyType(AddToEndSingleStrategy::class)
     fun showLoading()
 
     // Состояние ошибки
+    @StateStrategyType(AddToEndSingleStrategy::class)
     fun showError(errorMessage: String)
 
     // Состояние пустого списка
+    @StateStrategyType(AddToEndSingleStrategy::class)
     fun showEmpty(emptyMessage: String)
 
     // Состояние контента
+    @StateStrategyType(AddToEndSingleStrategy::class)
     fun showContent(movies: List<Movie>)
 
     // Методы одноразовых событий
-
+    @StateStrategyType(OneExecutionStateStrategy::class)
     fun showToast(additionalMessage: String)
-
-    fun render(state: MoviesState)
-
 
 }
